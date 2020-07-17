c2ecac2c17962b2bc8725815065ed42a495f4d47
jamesgallagher432 <jamesg@jamesg.app>
2020-07-17 08:48:58 +0100

    feat: Add help messages to commands

--- garfield_cli.py
+++ garfield_cli.py
@@ -9,10 +9,11 @@ import garfield
 def cli():
 	pass
 
-# Get today's comic
+# Comic command
 
 @cli.command()
 def today():
+	"""Retrieve today's Garfield comic."""
 	garfield.today()
 
 @cli.command()
@@ -20,20 +21,22 @@ def today():
 @click.option('-m', '--month', required=True, help="Month of the comic's release.", type=int)
 @click.option('-y', '--year', required=True, help="Year of the comic's release.", type=int)
 def fetch(day, month, year):
+	"""Retrieve a comic from the Garfield archive."""
 	garfield.fetch(day, month, year)
-
-# Get yesterday's comic
 
 @cli.command()
 def yesterday():
+	"""Retrieve yesterday's Garfield comic."""
 	garfield.yesterday()
 
 @cli.command()
 def random():
+	"""Retrieve a random comic from the Garfield archive."""
 	garfield.random()
 
 @cli.command()
 def first():
+	"""Retrieve the first comic from the Garfield archive."""
 	garfield.first()
 
 if __name__ == '__main__':
